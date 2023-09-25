ac945043171beba2a8e31de29b4a04e7bfbd9410
ArunJ03 <smilearun03@gmail.com>
2023-09-25 11:56:42 +0530

    Weather app is created.

--- src/Components/WeatherComponent/WeatherComponent.jsx
+++ src/Components/WeatherComponent/WeatherComponent.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
-import './WeatherComponent.css'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const WeatherComponent = () => {
+
+    const API_KEY = '76c33fccf408446ab4770123231409'
+    const [searchCity, setSearchCity] = useState('')
+    const [locationData, setLocationData] = useState([])
+    const [weatherData, setWeatherData] = useState([])
+    const [latitude, setLatitude] = useState()
+    const [longitude, setLongitude] = useState()
+
+
+    const handleSearch = async() => {
+        
+        const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${searchCity}`)
+        const data = response.data
+        setLocationData(data.location)
+        setWeatherData(data.current)    }
+
+    const handleSearchCity = (event) => {
+        setSearchCity(event.target.value)
+    }
+
+
+    useEffect( () => {
+        navigator.geolocation.getCurrentPosition( (position) => {
+            setLatitude(position.coords.latitude)
+            setLongitude(position.coords.longitude)
+        },
+        () => {
+            alert('Unable to retrieve location')
+        })
+    }, [])
+
+    useEffect( () => {
+        axios.get(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${latitude},${longitude}`)
+        .then( response  =>{
+            setLocationData(response.data.location)
+            setWeatherData(response.data.current)
+        })
+        console.log(locationData,weatherData)
+    }, [latitude,longitude])
+
   return (
-    <div>WeatherComponent</div>
+    <div>
+            <input 
+                type='text'
+                placeholder='Enter city'
+                value={searchCity}
+                onChange={handleSearchCity}
+            />
+            <button onClick={handleSearch}> Search </button>
+            <p>{locationData.name}</p>
+            <p>{weatherData.temp_c} &deg;C</p>
+            <p>{locationData.country}</p>
+        
+    </div>
   )
 }
 
